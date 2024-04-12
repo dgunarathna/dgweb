a97b08c2b7d79ae56ccce7b235a48f703e29e58b
uixdlg <dhanushkalakshan18@gmail.com>
2024-04-12 07:48:12 +0530

    done

--- js/app4.js
+++ js/app4.js
@@ -34,6 +34,64 @@ blocks.forEach(function (block) {
 
 
 
+function delay(n) {
+  n = n || 2000;
+  return new Promise((done) => {
+      setTimeout(() => {
+          done();
+      }, n);
+  });
+}
+
+function pageTransition() {
+  var tl = gsap.timeline();
+  tl.to(".loading-screen", {
+      duration: 1,
+      width: "100%",
+      left: "0%",
+      ease: "Expo.easeInOut",
+  });
+
+  tl.to(".loading-screen", {
+      duration: 1,
+      width: "100%",
+      left: "100%",
+      ease: "Expo.easeInOut",
+      delay: 0,
+  });
+  tl.set(".loading-screen", { left: "-100%" });
+}
+
+function contentAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
+}
+
+$(function () {
+  barba.init({
+      sync: true,
+
+      transitions: [
+          {
+              async leave(data) {
+                  const done = this.async();
+
+                  pageTransition();
+                  await delay(1000);
+                  done();
+              },
+
+              async enter(data) {
+                  contentAnimation();
+              },
+
+              async once(data) {
+                  contentAnimation();
+              },
+          },
+      ],
+  });
+});
 
 
 
@@ -51,7 +109,7 @@ gsap.from(" .block", 1.2, {
   width: "0px",
   // scale: 1,
   ease: "power3.inOut",
-  delay: 0,
+  delay:2,
   stagger: 0.2,
 });
 

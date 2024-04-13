9e35a1fab3ab008d69416e099762880c00bcf5dd
uixdlg <dhanushkalakshan18@gmail.com>
2024-04-13 16:46:13 +0530

    4.46

--- js/app4.js
+++ js/app4.js
@@ -412,3 +412,67 @@ video.addEventListener("canplay", function() {
     video.play();
   }, 1500);
 });
+
+
+//preloader
+
+
+gsap.from(".letter", 0.8,{
+  y: 20,
+  opacity: 0,
+  ease: "power3.inOut",
+  stagger: 0.1,
+});
+
+
+
+// gsap.to(".top-left, .top-right", 2,{
+//   top: "0",
+//   ease: "power3.inOut",
+//   delay: 2,
+// });
+
+gsap.to(".bottom-left, .bottom-right", 2,{
+  bottom: "0%",
+  ease: "power3.inOut",
+  delay: 2,
+});
+
+
+gsap.to(".top-left", 2,{
+  left: "-100",
+  ease: "power3.inOut",
+  delay: 4,
+});
+
+gsap.to(".top-right", 2,{
+  right: "-100",
+  ease: "power3.inOut",
+  delay: 4,
+  
+});
+
+gsap.to(".bottom-left", 2,{
+  left: "-100",
+  ease: "power3.inOut",
+  delay: 4,
+});
+
+gsap.to(".bottom-right", 2,{
+  right: "-100",
+  ease: "power3.inOut",
+  delay: 4,
+});
+
+gsap.to(".block-left", 2,{
+  left: "-50%",
+  ease: "power3.inOut",
+  delay: 4,
+});
+
+
+gsap.to(".block-right", 2,{
+  right: "-50%",
+  ease: "power3.inOut",
+  delay: 4,
+});
